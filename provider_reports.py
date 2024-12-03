b901d7f7703fa02fdf9cf72571dea1620b94f47c
Nikki Rudnick <186886169+nbrudnick@users.noreply.github.com>
2024-12-03 03:24:35 -0800

    function stubs

--- provider_reports.py
+++ provider_reports.py
@@ -8,4 +8,18 @@ from parent import parent
 
 
 class provider_reports(parent):
-    pass
+    def generate_provider_summary_report(): #high level menu option
+        #get date range of the summary report from the user
+        #find all doctor profiles will services logged in that range (probably a recursisve algorithim)
+        #as services in that range are found, copy service and doctor information to local variables
+
+
+        return
+    def generate_provider_service_report(): #high level menu option
+        return
+    def generate_EFT_report(): #high level menu option
+        return
+    def get_provider(): #get a provider the user wants to generate a provider service report for, and validate user input
+        return
+    def chronological_sort(): #sort services in a file by chronological order by day the service was provided
+        return
