574476c4c3b21472e61d0888da6634461e8a0f27
Catherine <c.n@pdx.edu>
2024-12-06 22:39:36 -0800

    cleaning

--- TODO.md
+++
@@ -1,5 +0,0 @@
-# TODO
-- Add three services to each demo profile
-- Pull repo changes
-- Upload demo profiles to git repo
-- Write tests for my code
--- mee.sh
+++
@@ -1,4 +0,0 @@
-while read -r line; do
-    file_path=$(echo "$line" | awk '{print $2}')
-    cp -- "$file_path" "../unique_files/"
-done < ../unique_checksum_list.txt
